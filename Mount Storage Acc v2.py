14821a4f0546c9a696f593a3e4b550cdccd2082d
ganesh-incedo <ganesh-incedo>
2023-08-02 20:32:12 +0000

    a
    s

---
+++ Mount Storage Acc v2.py
@@ -0,0 +1,22 @@
+# Databricks notebook source
+#v2
+storage_account="storageaccganesh"
+container_name="blobcontainer"
+source_url="wasbs://{0}@{1}.blob.core.windows.net".format(container_name,storage_account)
+
+access_key= ""
+mount_point_url="/mnt/gen1dataset"
+
+extra_configs_key=f"fs.azure.account.key.{storage_account}.blob.core.windows.net"
+extra_configs_value = access_key
+# extra_configs_dict = {extra_configs_key:extra_configs_value}
+
+# COMMAND ----------
+
+dbutils.fs.mount(source = source_url, 
+                 mount_point = mount_point_url,
+                 extra_configs= {extra_configs_key:extra_configs_value})
+
+# COMMAND ----------
+
+dbutils.fs.ls(mount_point_url)
